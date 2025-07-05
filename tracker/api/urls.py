016b43572a8a9e8f5e71b720c41ed1633fa0ca64
shahram <shahramsamar2010@gmail.com>
2025-07-05 11:28:04 +0330

    Create urls.py
    urls for api

---
+++ tracker/api/urls.py
@@ -0,0 +1,20 @@
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+from .views import ExpenseIncomeViewSet,UserRegisterView
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
+
+router = DefaultRouter()
+router.register(r'expenses', ExpenseIncomeViewSet, basename='expenses')
+
+urlpatterns = [
+    path('', include(router.urls)),
+    # authentication and registration URLs
+    path('auth/register/', UserRegisterView.as_view(), name='register'),
+    path('auth/login/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('auth/refresh/', TokenRefreshView.as_view(), name='token_refresh'), 
+    
+    path()
+    
+    
+    
+]
