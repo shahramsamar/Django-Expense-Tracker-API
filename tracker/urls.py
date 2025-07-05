898da2861efde6d4d21e9fa0f27b40e523bbd6df
shahram <shahramsamar2010@gmail.com>
2025-07-05 11:27:17 +0330

    added crude in view

--- tracker/urls.py
+++ tracker/urls.py
@@ -1,6 +1,12 @@
-from django.urls import path,include
+from django.urls import path, include
+from tracker import views
+
+
 
 urlpatterns = [
- 
-    
+    path('', views.ExposeListView.as_view(), name="expose-list"),
+    path('expose/<int:pk>/', views.ExposeDetailView.as_view(), name="expose-detail"),
+    path('expose/new/', views.ExposeCreateView.as_view(), name="expose-create"),
+    path('expose/<int:pk>/update/', views.ExposeUpdateView.as_view(), name="expose-update"),
+    path('expose/<int:pk>/delete/', views.ExposeDeleteView.as_view(), name="expose-delete"),
 ]
--- tracker/views.py
+++ tracker/views.py
@@ -1,3 +1,51 @@
-from django.shortcuts import render
+from django.forms import BaseModelForm
+from django.http import HttpResponse
+from .models import ExpenseIncome
+from django.views.generic import(
+    ListView,
+    DeleteView,
+    CreateView,
+    UpdateView,
+    DetailView )
 
-# Create your views here.
+
+
+
+class ExposeListView(ListView):
+    model = ExpenseIncome
+    template_name = 'expose_list.html'
+    context_object_name = 'exposes'
+
+
+class ExposeDetailView(DetailView):
+    model = ExpenseIncome
+    template_name = 'expose_detail.html'
+    context_object_name = 'expose'
+    
+class ExposeCreateView(CreateView):
+    model = ExpenseIncome
+    template_name = 'expose_form.html'
+    fields = ['title', 'description', 'amount','transaction_type','tax','tax_type']
+    success_url = '/'
+    
+    def form_valid(self, form):
+        self.object = form.save(commit=False)
+        self.object.user = self.request.user
+        return super().form_valid(form)
+
+class ExposeUpdateView(UpdateView):
+    model = ExpenseIncome
+    template_name = 'expose_form.html'
+    fields = ['title', 'description', 'amount','transaction_type','tax','tax_type']
+    success_url = '/'
+
+    def form_valid(self, form):
+        self.object = form.save(commit=False)
+        self.object.user = self.request.user
+        return super().form_valid(form)
+
+class ExposeDeleteView(DeleteView):
+    model = ExpenseIncome
+    template_name = 'expose_confirm_delete.html'
+    success_url = '/'
+    
