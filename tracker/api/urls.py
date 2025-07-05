ed63d7012b758b87381dba47557dbdf8c24a6db6
shahram <shahramsamar2010@gmail.com>
2025-07-05 11:46:56 +0330

    Update urls.py

--- tracker/api/urls.py
+++ tracker/api/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from .views import ExpenseIncomeViewSet,UserRegisterView
+from ..api.views import ExpenseIncomeViewSet,UserRegisterView
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
+
 
 router = DefaultRouter()
 router.register(r'expenses', ExpenseIncomeViewSet, basename='expenses')
@@ -11,10 +12,5 @@ urlpatterns = [
     # authentication and registration URLs
     path('auth/register/', UserRegisterView.as_view(), name='register'),
     path('auth/login/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
-    path('auth/refresh/', TokenRefreshView.as_view(), name='token_refresh'), 
-    
-    path()
-    
-    
-    
+    path('auth/refresh/', TokenRefreshView.as_view(), name='token_refresh'),  
 ]
